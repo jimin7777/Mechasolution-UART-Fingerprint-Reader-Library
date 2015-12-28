9e8edfc02b42f4414964ae9d39312bcde5331730
hong gil dong <yev_science@naver.com>
2015-12-28 16:04:27 +0900

    Update simple.ino

--- example/simple/simple.ino
+++ example/simple/simple.ino
@@ -3,26 +3,28 @@
  */
 #include <SoftwareSerial.h>
 #include <MECHA_UARTFingerprint.h>
+#define NEW_USER_ID 1 //새로 등록할 사용자의 아이디를 이곳에서 설정합니다.
+#define NEW_USER_PRIV 3 //새로 등록할 사용자의 권한을 이곳에서 설정합니다.
 SoftwareSerial sofSeri(2,3);
 MECHA_UARTFinger fin = MECHA_UARTFinger(&sofSeri);
 void setup() {
   Serial.begin(9600);
   fin.begin();
-  fin.AddUser1(1,3);
-  //1은 사용자 ID, 3은 사용자 권한. AddUser1,2,3 3회 반복
-  //the 1 is user ID, 3 is users Privilege. AddUser1,2,3  3times repeat
+  fin.AddUser1(NEW_USER_ID,NEW_USER_PRIV);
+  //NEW_USER_ID는 사용자 ID, NEW_USER_PRIV은 사용자 권한. AddUser1,2,3 3회 반복
+  //the NEW_USER_ID is user ID, NEW_USER_PRIV is users Privilege. AddUser1,2,3  3times repeat
   fin.GetRespon();
   //반환값을 수신합니다. 함수마다 계속 반복하는것을 추천드립니다.
   //always have the Respon
   if(ACK_SUCCESS == fin.GetFeedback()){
     Serial.println("Success add user 1/3");
   }
-  fin.AddUser2(1,3);
+  fin.AddUser2(NEW_USER_ID,NEW_USER_PRIV);
   fin.GetRespon();
   if(ACK_SUCCESS == fin.GetFeedback()){
     Serial.println("Success add user 2/3");
   }
-  fin.AddUser3(1,3);
+  fin.AddUser3(NEW_USER_ID,NEW_USER_PRIV);
   fin.GetRespon();
   if(ACK_SUCCESS == fin.GetFeedback()){
     Serial.println("Success add user 3/3");
